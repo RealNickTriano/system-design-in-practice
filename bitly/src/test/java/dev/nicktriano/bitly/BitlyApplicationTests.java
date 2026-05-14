9c5f0c28fba3ca9b5b9fe890573b88b101548649
RealNickTriano <nicholastriano1999@gmail.com>
2026-05-14 11:06:13 -0400

    fixed test container issue

--- bitly/src/test/java/dev/nicktriano/bitly/BitlyApplicationTests.java
+++ bitly/src/test/java/dev/nicktriano/bitly/BitlyApplicationTests.java
@@ -2,9 +2,18 @@ package dev.nicktriano.bitly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BitlyApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17");
 
 	@Test
 	void contextLoads() {
